e83418e678820771250eb7aa1065def82b8cb838
Nathan Henry <na.henry@outlook.com>
2020-02-23 15:40:45 +0100

    [MariaDB](feature) Deleted sql file - Setting up the ORM

--- data_db/setup.sql
+++
@@ -1,4 +0,0 @@
-create table if not exists table1(a int not null);
-create table if not exists table2(b int not null);
-create table if not exists table3(c int not null);
-create table if not exists table4(d int not null);
